1b32b28ecbaafa2d74dd89140c7c50b753f1b163
jonathandagoberto <jonathandagoberto1@gmail.com>
2023-05-22 20:39:14 -0500

    Mensaje de commit descriptivo

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+    // Validar la identidad del usuario contra Corabastic
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Realizar la validación y determinar si hay errores
+    $errors = array();
+    if (empty($username)) {
+        $errors[] = "El campo de usuario no puede estar vacío.";
+    }
+
+    if (empty($password)) {
+        $errors[] = "El campo de contraseña no puede estar vacío.";
+    }
+
+    // Mostrar los errores, si los hay
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo "<p class='error'>$error</p>";
+        }
+    } else {
+        // Procesar el inicio de sesión exitoso
+        echo "Inicio de sesión exitoso. Bienvenido, $username!";
+    }
+?>
